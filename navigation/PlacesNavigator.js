4277220d2ce610398a9ee0708c214e699d1f24f4
javier <javier@dojodevs.io>
2020-06-22 01:13:52 -0500

    first commit

---
+++ navigation/PlacesNavigator.js
@@ -0,0 +1,6 @@
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+
+import PlacesListScreen from '../screens/PlacesListScreen';
+import PlaceDetailScreen from '../screens/PlaceDetailScreen';
+import NewPlaceScreen from '../screens/NewPlaceScreen';
+import MapScreen from '../screens/MapScreen';
